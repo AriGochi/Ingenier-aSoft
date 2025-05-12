4a7435919564fcb768244635a59a8eb658131ebc
Ariadna Gochi Gutiérrez <125834764+AriGochi@users.noreply.github.com>
2025-05-11 22:52:17 -0600

    Add files via upload

---
+++ computadoraApi/computadoraApi/src/main/java/mx/unam/aragon/ico/is/computadoraApi/repositories/ComputadoraRepository.java
@@ -0,0 +1,11 @@
+package mx.unam.aragon.ico.is.computadoraApi.repositories;
+
+import mx.unam.aragon.ico.is.computadoraApi.entitys.Computadora;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ComputadoraRepository extends CrudRepository<Computadora, Long> {
+
+    public Computadora findComputadoraByClave(Long clave);
+
+    public Computadora deleteByClave(Long clave);
+}
